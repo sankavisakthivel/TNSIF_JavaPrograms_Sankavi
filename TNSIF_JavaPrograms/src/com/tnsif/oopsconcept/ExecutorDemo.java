fccefcf7e926ad4bec2acefdf10863e154ac741b
Dell <Dell@DESKTOP-K098B5P>
2025-07-03 09:51:12 +0530

    commit 2

---
+++ TNSIF_JavaPrograms/src/com/tnsif/oopsconcept/ExecutorDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.oopsconcept;
+public class ExecutorDemo {
+public static void main(String[] args) {
+EncapsulationDemo ed = new EncapsulationDemo();
+ed.setName("MS Dhoni");
+ed.setAge(43);
+ed.setJersyNo(7);
+System.out.println(ed);
+}
+}
